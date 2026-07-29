c853f781118c2489a196055e04cf33c43cfe183f
Asmaa <asmaamouhouche2007@gmail.com>
2026-07-29 23:04:06 +0200

    adding

--- project_queries/query_1.sql
+++ project_queries/query_1.sql
@@ -1,1 +1,19 @@
--- query 1 
+-- the first question we will answer is :
+-- what are the top 10 highest-paying data 
+--analyst roles that are available remotely ?
+
+SELECT 
+cd.name AS company_name,
+jpf.job_title,
+jpf.job_title,
+jpf.job_schedule_type,
+jpf.salary_year_avg,
+jpf.job_posted_date
+FROM job_postings_fact jpf
+JOIN company_dim cd
+ON jpf.company_id=cd.company_id
+AND trim(lower(jpf.job_title_short))='data analyst'
+AND lower(TRIM(jpf.job_location))='anywhere'
+AND jpf.salary_year_avg IS NOT NULL
+ORDER BY jpf.salary_year_avg DESC
+LIMIT 10;
